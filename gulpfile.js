8ebda179f0d625fde831b3ae45ee6343cbc1b99c
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-04-13 16:23:26 -0300

    Finalizei os artigos do mobile first

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 let gulp = require('gulp')
 let sass = require('gulp-sass')(require('sass'))
 let sourcemaps = require('gulp-sourcemaps')
+let imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -12,6 +13,13 @@ function styles() {
         .pipe(gulp.dest('./build/styles'))
 }
 
+function imgMin() {
+    return gulp.src('./src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+
 exports.default = function() {
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(styles))
+    gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.series(imgMin))
 }
